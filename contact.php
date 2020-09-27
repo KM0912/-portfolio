b29c32b38b86e661b45c876b468c2b9cbea61893
KM0912 <61630275+KM0912@users.noreply.github.com>
2020-09-27 15:10:49 +0900

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+        <title>コンタクトフォーム</title>
+            <!--<link href="../bootstrap4/css/bootstrap.min.css" rel="stylesheet">
+            <link href="style.css" rel="stylesheet"> -->
+    </head>
+
+    <body>
+        <div class="container">
+            <h2>お問い合わせフォーム</h2>
+        </div>
+
+    </body>
+</html>
